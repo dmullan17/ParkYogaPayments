a61c34d008fa4cc34c27829c7cbf2fd421acb800
dmullan17 <43132178+dmullan17@users.noreply.github.com>
2024-02-16 23:27:08 +0000

    remove custom web root for folder

--- Server.cs
+++ Server.cs
@@ -24,7 +24,7 @@ namespace server.Controllers
         {
             WebHost.CreateDefaultBuilder(args)
               //.UseUrls("http://0.0.0.0:4242")
-              .UseWebRoot("public")
+              //.UseWebRoot("public")
               .UseStartup<Startup>()
               .Build()
               .Run();
